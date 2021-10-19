c25ec3a4c2d57453e264560d0d07bd93dc415d19
hcarreiro <32105829+hcarreiro@users.noreply.github.com>
2021-10-19 18:26:47 -0300

    Update UnitTest1.cs
    Dá uma olhada nas minhas observações nos comentários, veja se são pertinentes. Caso sim, vamos implementar essas melhorias.

--- minhaAutomacao/UnitTest1.cs
+++ minhaAutomacao/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace minhaAutomacao
     {
         public static string chromeWebDriver = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-        // URL da p�gina a ser testada
+        // URL da página a ser testada
         public static string linkSiteProjeto = "http://www.casaevideo.com.br/";
 
         [TestMethod]
@@ -37,18 +37,27 @@ namespace minhaAutomacao
             IWebElement acessarMinhaLista = driver.FindElement(By.XPath("/html/body/div[3]/header/div[2]/div/div/div[6]/div[1]/div/ul/li[3]/a/u"));
             acessarMinhaLista.Click();
 
+            //-------------------Inserir um Assert para validar se vc está na página de Login
+            
+            //-------------------Esta propriedade inserirEmail na verdade não é um "botão" de Cadastro?
             IWebElement inserirEmail = driver.FindElement(By.Id("header-sign-up-link"));
             inserirEmail.Click();
 
+            //-------------------Inserir um Assert para validar se vc está na página de Cadastro
+            
+            //-------------------Implementar um método para gerar nomes e usar aqui
             IWebElement nome = driver.FindElement(By.Id("pre-sign-up-form-first-name"));
-            nome.SendKeys("Andr�");
+            nome.SendKeys("André");
 
+            //-------------------Implementar um método para gerar e-mails e usar aqui - será quase o mesmo do nome ;-) 
             IWebElement email = driver.FindElement(By.Id("pre-sign-up-form-email"));
             email.SendKeys("exemplo304@exemplo.com");
 
+            //-------------------Implementar um método para gerar CPF e usar aqui
             IWebElement cpf = driver.FindElement(By.Id("pre-sign-up-form-cpf"));
             cpf.SendKeys("543.917.440-07");
-
+            
+            //-------------------Implementar um método para gerar senha e usar aqui
             IWebElement senha = driver.FindElement(By.Id("pre-sign-up-form-password"));
             senha.SendKeys("Teste@Teste");
 
@@ -58,6 +67,7 @@ namespace minhaAutomacao
             IWebElement cadastrar = driver.FindElement(By.Id("btn-pre-sign-up-new"));
             cadastrar.Click();
 
+            
             resultadoAtual = driver.Url;
 
             if (resultadoAtual.Contains(resultadoEsperado))
